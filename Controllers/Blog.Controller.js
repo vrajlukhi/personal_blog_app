b2ba3dac18a0be3306ee6123615305985d6ed435
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2024-09-18 05:25:56 +0530

    try catch

--- Controllers/Blog.Controller.js
+++ Controllers/Blog.Controller.js
@@ -1,40 +1,84 @@
-const BlogModel = require("../Models/Blog.Schema.Js")
+const BlogModel = require("../Models/Blog.Schema.Js");
 
 const HomeUi = (req, res) => {
-    res.render("home")
-}
-const AddbogUi = (req, res) => {
-    res.render("addblog")
-}
-const AddBlog = async (req, res) => {
-    let data = await BlogModel.create(req.body)
-    res.redirect("/blog/home")
-}
-const AllBlog = async (req, res) => {
-    let data = await BlogModel.find().sort({ createdAt: -1 })
-    res.json(data)
-}
-const singleblog = async (req, res) => {
-    let { id } = req.params
-    let data = await BlogModel.findById(id)
-    res.render("singleblog", { data })
-
-}
-const deletedata = async (req, res) => {
-    let { id } = req.params
-    await BlogModel.findByIdAndDelete(id)
-    res.redirect("/blog/home")
-}
-const Editdata = async (req, res) => {
-    let { id } = req.params
-    const data = await BlogModel.findById(id)
-    res.render("editblog", { data })
-}
-const updatedata = async (req, res) => {
-    let { id } = req.params;
-    let listing = await BlogModel.findByIdAndUpdate(id, req.body);
-
-    res.redirect(`/blog/singleblog/${id}`);
+    res.render("home");
 };
 
-module.exports = { HomeUi, AddbogUi, AddBlog, AllBlog, singleblog, deletedata, Editdata, updatedata }
+const AddbogUi = (req, res) => {
+    res.render("addblog");
+};
+
+const AddBlog = async (req, res) => {
+    try {
+        await BlogModel.create(req.body);
+        res.redirect("/blog/home");
+    } catch (error) {
+        console.error("Error adding blog:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const AllBlog = async (req, res) => {
+    try {
+        const data = await BlogModel.find().sort({ createdAt: -1 });
+        res.json(data);
+    } catch (error) {
+        console.error("Error fetching all blogs:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const singleblog = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await BlogModel.findById(id);
+        if (!data) {
+            return res.status(404).send("Blog not found");
+        }
+        res.render("singleblog", { data });
+    } catch (error) {
+        console.error("Error fetching single blog:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const deletedata = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await BlogModel.findByIdAndDelete(id);
+        res.redirect("/blog/home");
+    } catch (error) {
+        console.error("Error deleting blog:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const Editdata = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = await BlogModel.findById(id);
+        if (!data) {
+            return res.status(404).send("Blog not found");
+        }
+        res.render("editblog", { data });
+    } catch (error) {
+        console.error("Error fetching blog for editing:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+const updatedata = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const listing = await BlogModel.findByIdAndUpdate(id, req.body, { new: true });
+        if (!listing) {
+            return res.status(404).send("Blog not found");
+        }
+        res.redirect(`/blog/singleblog/${id}`);
+    } catch (error) {
+        console.error("Error updating blog:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+module.exports = { HomeUi, AddbogUi, AddBlog, AllBlog, singleblog, deletedata, Editdata, updatedata };
