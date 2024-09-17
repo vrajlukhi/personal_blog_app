20aa54a507c9eea75e278db5c0f9fabd068ce623
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2024-09-18 04:41:45 +0530

    connection

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 const connect=async()=>{
-    await mongoose.connect(process.env.SERVER)
+    await mongoose.connect("mongodb+srv://vrajlukhi:vrajlukhi@cluster0.h0qqzcb.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0")
     console.log("Mongoose connected");
 }
 module.exports=connect
